c52ae30925096289fe4d7cb05bc0aa5628f6c9d9
Yizhou Wang <yizhouwang418@gmail.com>
2021-02-21 21:09:15 +0800

    1

---
+++ language_basic/language_basic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace language_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
